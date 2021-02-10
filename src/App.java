eb7672649daab104d4bb101c144658bcf6f47939
Gitea <gitea@fake.local>
2021-02-11 01:39:41 +0530

    added for loop

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("I love VS Code");
-        System.out.println("Commiting to teh git");
+        for(int i=1; i<=10; i++)
+        System.out.println("Hello, World!..."+  i);
+        
     }
 }

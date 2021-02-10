6712c72b626f970fbf504908d3cd66cf629e6afe
Gitea <gitea@fake.local>
2021-02-11 01:14:10 +0530

    Added new line

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("I love VS Code");
+        System.out.println("Commiting to teh git");
     }
 }

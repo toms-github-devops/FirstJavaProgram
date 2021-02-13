e539af1212b6c0ba4b8b2c2ff18377c48e6d6179
Gitea <gitea@fake.local>
2021-02-13 10:32:04 +0530

    Added new line

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) {
         for(int i=1; i<=10; i++)
         System.out.println("Hello, World!..."+  i);
-        
+        System.out.println("I love VSCode");
     }
 }
